b665bfdad7389f9dc54de10e2447610d772c7855
ongjx <ongjx9@hotmail.com>
2018-04-29 21:52:28 +0800

    included export csv function

--- adidas/spiders/spider.py
+++ adidas/spiders/spider.py
@@ -1,14 +1,18 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
 from adidas.items import AdidasItem
 import requests
 import csv
 import pandas
 
-class BlogSpider(scrapy.Spider):
+class AdidasSpider(scrapy.Spider):
     name = 'adidas'
     allowed_domains = ['adidas.com.sg']
     start_urls = ['http://shop.adidas.com.sg/catalogsearch/result/index/?limit=120']
-
+    custom_settings = {
+        'FEED_FORMAT': 'csv',
+        'FEED_URI': 'adidas3.csv'
+    }
     def parse(self, response):
         soup = response.xpath("//*[@class='product-info-wrapper']")
         for prod in soup:
@@ -18,12 +22,17 @@ class BlogSpider(scrapy.Spider):
             item['price'] = prod.xpath('div[@class="price-box"]/p/span/@pricerounded').extract_first()
             item['discounted_price'] = prod.xpath('div[@class="price-box"]/p[@class="special-price"]/span/@pricerounded').extract_first()
             yield item
-            
+
         nextPageLinkSelect = response.xpath('//div[@class="prevnextbuttons"]/a[2]/@href')
         if nextPageLinkSelect:
             nextPageLink = nextPageLinkSelect.extract_first()
             print(nextPageLink)
             yield scrapy.Request(url=nextPageLink, callback=self.parse)
-        
 
+
+
+process = CrawlerProcess({'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'})
+
+process.crawl(AdidasSpider)
+process.start()
 
